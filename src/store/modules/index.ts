e99f9066aa3ccba31de949ffa47afaacd851888a
Jaloliddin Musabayev <musaboyev6@gmail.com>
2024-10-21 10:59:22 +0500

    init

---
+++ src/store/modules/index.ts
@@ -0,0 +1,40 @@
+const state = {
+    isAuthenticated: false,
+    user: null
+};
+  
+const mutations = {
+    SET_USER(state, user) {
+        state.user = user;
+        state.isAuthenticated = true;
+    },
+    CLEAR_USER(state) {
+        state.user = null;
+        state.isAuthenticated = false;
+    }
+};
+  
+const actions = {
+    login({ commit }, user) {
+        // Вызывается при успешной авторизации
+        commit('SET_USER', user);
+    },
+    logout({ commit }) {
+        // Очищает состояние при выходе пользователя
+        commit('CLEAR_USER');
+    }
+};
+  
+const getters = {
+    isAuthenticated: (state) => state.isAuthenticated,
+    user: (state) => state.user,
+    userName: (state): string | null => state.user ? state.user.name : null
+};
+  
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions,
+    getters
+};
